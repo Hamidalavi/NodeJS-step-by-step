9a9dc6facadb7d121a0769488c922215667b86ce
Hamid Alavi <ultihamid@gmail.com>
2022-08-12 17:45:28 +0430

    add another tutorials

---
+++ Learn/built-in functions.js
@@ -0,0 +1,9 @@
+// random number
+// const string = "String";
+// const number;
+
+// string.substr(number, number); // cuts the string from beginning and the rest of the length (index of string)
+
+// const generateRandomString = (length = 12) => {
+//   Math.random().toString(20).substr(2, length);
+// };
